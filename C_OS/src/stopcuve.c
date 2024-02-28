b93bd969035fa0408f1eb96ca855a155e75caf36
Clarice Goulet <kimi.goulet@gmail.com>
2024-02-28 09:54:44 +0100

    stopcuve

--- C_OS/src/stopcuve.c
+++ C_OS/src/stopcuve.c
@@ -1,6 +1,5 @@
 /*====================================================*/
-/* programme pour la modification de la hauteur voulue*/
-/* dans la cuve.                                      */
+/* programme pour l'arrêt des programmes              */
 /*____________________________________________________*/
 /* Jacques BOONAERT-LEPERS  evaluation AMSE 2023-2024 */
 /*====================================================*/
@@ -20,32 +19,11 @@
 /* constante de l'application */
 /*............................*/
 #define STOPCUVE "STOPCUVE"
-/*...................*/
-/* prototypes locaux */
-/*...................*/
-void usage(char *); /* ->aide de ce programme                */
 /*#####################*/
 /* programme principal */
 /*#####################*/
 int main(int argc, char *argv[])
 {
-    double valeur; /* ->valeur a ecrire dans la zone  */
-    double *yt;    /* ->pointeur sur la zone partagee */
-    int fh;        /* ->"file descriptor" sur la zone partagee */
-    /* verification des arguments */
-    if (argc != 2)
-    {
-        usage(argv[0]);
-        return (0);
-    };
-    /* recuperation des arguments */
-    if (sscanf(argv[1], "%lf", &valeur) == 0)
-    {
-        printf("ERREUR : probleme de format des arguments\n");
-        printf("         passe en ligne de commande.\n");
-        usage(argv[0]);
-        return (0);
-    };
     /*................*/
     /* initialisation */
     /*................*/
@@ -66,7 +44,7 @@ int main(int argc, char *argv[])
                         MAP_SHARED,
                         fh,
                         0);
-    *yt = valeur; /* ->ecriture effective dans la zone partagee */
+    *yt = 1; /* ->ecriture effective dans la zone partagee */
     close(fh);
     return (0); /* ->on n'arrive pas jusque la en pratique */
 }
